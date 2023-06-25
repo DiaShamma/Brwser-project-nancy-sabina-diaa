5a7307f599d56ec649f9e478fe564793e7fd8e39
Diaa Shamma <125613718+DiaShamma@users.noreply.github.com>
2023-06-25 21:28:47 +0200

    Update scoreView.js

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -32,7 +32,7 @@ export const updateScoreCounter = (score) => {
 
   if (currentQuestion.selected === currentQuestion.correct) {
     const correctImage = document.createElement('img');
-    correctImage.src = 'https://github.com/DiaShamma/Brwser-project-nancy-sabina-diaa/tree/main/public/images'
+    correctImage.src = '/public/images/correct.png'
     correctImage.alt = 'Right Answer';
     correctImage.classList.add('slide-in'); // Add a fade-in animation class
     answerIndicator.appendChild(correctImage);
